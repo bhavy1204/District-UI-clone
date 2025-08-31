7d9505c17cc09b8649d9c2081cb891d0977e1b4a
bhavy1204 <bhavysarwa51@gmail.com>
2025-08-31 20:11:09 +0530

    initial project setup complete

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1 className='text-green-500 text-3xl'>
+        Hello Activites
+      </h1>
+    </>
+  )
+}
+
+export default App
